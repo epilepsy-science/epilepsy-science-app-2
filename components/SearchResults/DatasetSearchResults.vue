b2a1b7391e3f651a596ec8acba63b181d4b63b35
vijay kumar, samayamantula <vijay@sparc.science>
2024-08-23 17:59:31 -0400

    fix sass console errors

--- components/SearchResults/DatasetSearchResults.vue
+++ components/SearchResults/DatasetSearchResults.vue
@@ -206,14 +206,15 @@ export default {
   }
 }
 .property-table {
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  
   td {
     background-color: transparent;
     padding: 0.25rem 0 0 0;
     border: none;
   }
-  background-color: transparent;
-  border: none;
-  padding: 0;
 }
 // The outermost bottom border of the table. Element UI adds psuedo elements to create the bottom table border that we must hide to remove
 table:not([class^='el-table__'])::before {
