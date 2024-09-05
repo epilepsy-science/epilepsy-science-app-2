e9726359cb330ec924ae7f907b3f51f2dccb0da7
vijay kumar, samayamantula <vijay@sparc.science>
2024-09-05 13:41:21 -0400

    update metadata and remove unused pages

--- pages/projects/[projectId].vue
+++
@@ -1,9 +0,0 @@
-<script>
-export default {
-  async setup() {
-    const route = useRoute()
-    const newPath = route.href.replace('/projects', '/about/projects')
-    await navigateTo(newPath, { redirectCode: 301 })
-  },
-}
-</script>
