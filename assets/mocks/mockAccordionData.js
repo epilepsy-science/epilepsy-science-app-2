b42e7ba0a58be6c6ba4915e268c87b09d54326c0
vijay kumar, samayamantula <vijay@sparc.science>
2024-11-15 21:49:28 -0500

    organize data

--- assets/mocks/mockAccordionData.js
+++ assets/mocks/mockAccordionData.js
@@ -5,10 +5,10 @@ export const mockAccordionData = [
       title: 'Access Global Research Data',
       description: 'Discover data from scientists across the world from the largest brain scan data repository in the world.',
       imgSrc: 'https://images.ctfassets.net/erzgaqq17mnz/46BUPtc8c1AJXcI8kpHrN4/8b2a49b4f55525c675330c6f4c9a55a9/epilepsy.science.png',
-      redirectLink1Text: 'Learn More',
-      redirectLink2Text: 'Access Research Portal',
-      redirectLink1Url: '/data',
-      redirectLink2Url: '/portal/access'
+      redirectLink1Text: 'Access Research Portal',
+      redirectLink2Text: 'Learn More',
+      redirectLink1Url: '/portal/access',
+      redirectLink2Url: '/data'
     }
   },
   {
@@ -17,10 +17,10 @@ export const mockAccordionData = [
       title: 'Build Custom Data Cohorts',
       description: 'Create cohorts of data from across multiple datasets worldwide. Choose from filters such as age, sex and other medical factors',
       imgSrc: 'https://images.ctfassets.net/erzgaqq17mnz/46BUPtc8c1AJXcI8kpHrN4/8b2a49b4f55525c675330c6f4c9a55a9/epilepsy.science.png',
-      redirectLink1Text: 'Coming soon!',
-      redirectLink2Text: 'Explore Cohort Tools',
-      redirectLink1Url: '/',
-      redirectLink2Url: '/cohorts/tools'
+      redirectLink1Text: 'Explore Cohort Tools',
+      redirectLink2Text: 'Coming soon!',
+      redirectLink1Url: '/cohorts/tools',
+      redirectLink2Url: '/'
     }
   },
   {
@@ -29,10 +29,10 @@ export const mockAccordionData = [
       title: 'Comprehensive Data Download Options',
       description: 'Search and download epilepsy datasets',
       imgSrc: 'https://images.ctfassets.net/erzgaqq17mnz/46BUPtc8c1AJXcI8kpHrN4/8b2a49b4f55525c675330c6f4c9a55a9/epilepsy.science.png',
-      redirectLink1Text: 'Download Data',
-      redirectLink2Text: 'More on Data Format',
-      redirectLink1Url: '/data',
-      redirectLink2Url: '/data/format-info'
+      redirectLink1Text: 'More on Data Format',
+      redirectLink2Text: 'Download Data',
+      redirectLink1Url: '/data/format-info',
+      redirectLink2Url: '/data'
     }
   },
   {
@@ -41,10 +41,10 @@ export const mockAccordionData = [
       title: 'Share Your Research Data',
       description: 'Add your data to the repository, link to it in papers, share with other scientists to advance epilepsy research.',
       imgSrc: 'https://images.ctfassets.net/erzgaqq17mnz/46BUPtc8c1AJXcI8kpHrN4/8b2a49b4f55525c675330c6f4c9a55a9/epilepsy.science.png',
-      redirectLink1Text: 'Contribute Now',
-      redirectLink2Text: 'Learn About Data Sharing',
-      redirectLink1Url: 'https://forms.gle/rGW9MQna5jQa7iGm6',
-      redirectLink2Url: '/data/sharing-info'
+      redirectLink1Text: 'Learn About Data Sharing',
+      redirectLink2Text: 'Contribute Now',
+      redirectLink1Url: '/data/sharing-info',
+      redirectLink2Url: 'https://forms.gle/rGW9MQna5jQa7iGm6'
     }
   }
 ]
