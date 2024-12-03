4ee9873192314071fcacc229ac691c1a49dd8f9d
vijay kumar, samayamantula <vijay@sparc.science>
2024-12-03 13:31:31 -0500

    refactor about data

---
+++ assets/content/aboutPageContent.js
@@ -0,0 +1,32 @@
+export const aboutPageContent = {
+  mission: {
+    title: "Our mission is to open epilepsy science and share it for public & scientific collaboration.",
+  },
+  statsDescription:
+    "The platform provides over 200,000 EEG recordings from diverse contexts including routine outpatient EEGs, critically ill patients, and epilepsy monitoring unit evaluations.",
+  whatWeDo: {
+    title: "What are we doing here at Epilepsy.Science?",
+    textItems: [
+      "Epilepsy.Science is a new cloud-based platform for managing, analyzing, publishing, and sharing scientific datasets to accelerate epilepsy research.",
+      "Led by PennSieve and the Brain Data Science Platform (BDSP) and with support from the AWS Open Data Sponsorship Program, we are creating an unparalleled open data resource for the epilepsy community.",
+    ],
+  },
+  missionDetails: {
+    title: "Driving Progress is our Mission",
+    subtitle:
+      "We strive to understand, treat, and ultimately cure epilepsy through open access to multidimensional epilepsy data at scale. The platform provides over 200,000 EEG recordings from diverse contexts including routine outpatient EEGs, critically ill patients, and epilepsy monitoring unit evaluations.",
+    additionalInfo:
+      "As the platform grows, it will also include extensive accompanying clinical data like medications, imaging, genetics, and more from institutions worldwide.",
+    introduction:
+      "Researchers can use Epilepsy.Science to easily build customized cohorts by connecting data points across datasets.The platform enables scientists to publish -- at no cost -- high quality datasets for citation, reuse, and reproducible research.By promoting open science, Epilepsy.Science aims to accelerate discoveries and improve patient outcomes.",
+    highlightsTitle: "This collaboration brings together:",
+    highlights: [
+      "PennSieve's scalable data management and sharing capabilities.",
+      "BDSP's extensive data resources including over 200,000 EEG recordings.",
+      "The AWS Open Data Sponsorship Program covers the cost of storage.",
+      "Epilepsy.Science offers unprecedented opportunities for open, collaborative epilepsy research.",
+    ],
+    imageSrc: "https://images.ctfassets.net/erzgaqq17mnz/25miXcUrrPaHjdX0dJ4ce6/8efe96f5a3723a404f4a6a33dbb70d18/mission-details-image.png",
+    footer: "Epilepsy.Science offers unprecedented opportunities for open, collaborative epilepsy research through its powerful data resources, analytics tools, and cloud-based platform."
+  },
+};
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -7,7 +7,7 @@
     <section class="body-wrapper stats-section">
       <div class="stats-container">
         <el-row class="stats-row">
-          <el-col v-for="(stat, index) in content.stats" :key="index" :span="6" :xs="12" :md="6" class="stat-box">
+          <el-col v-for="(stat, index) in displayStats" :key="index" :span="6" :xs="12" :md="6" class="stat-box">
             <div class="stat-value">{{ stat.value }}</div>
             <div class="stat-label">{{ stat.label }}</div>
           </el-col>
@@ -54,46 +54,19 @@
 
 <script setup>
 import { useMainStore } from '~/store/index';
+import { aboutPageContent } from '../../assets/content/aboutPageContent';
+import { ref } from 'vue';
 
 const stats = useMainStore().pageStats
-const content = {
-  mission: {
-    title: "Our mission is to open epilepsy science and share it for public & scientific collaboration.",
-  },
-  stats: [
+const content = ref(aboutPageContent)
+
+const displayStats = [
     { value: `${stats.files}`, label: "Files" },
     { value: `${stats.labs}+`, label: "Labs" },
     { value: `${stats.datasets}`, label: "Datasets" },
     { value: `${stats.publicUsers}+`, label: "Public Users" },
-  ],
-  statsDescription:
-    "The platform provides over 200,000 EEG recordings from diverse contexts including routine outpatient EEGs, critically ill patients, and epilepsy monitoring unit evaluations.",
-  whatWeDo: {
-    title: "What are we doing here at Epilepsy.Science?",
-    textItems: [
-      "Epilepsy.Science is a new cloud-based platform for managing, analyzing, publishing, and sharing scientific datasets to accelerate epilepsy research.",
-      "Led by PennSieve and the Brain Data Science Platform (BDSP) and with support from the AWS Open Data Sponsorship Program, we are creating an unparalleled open data resource for the epilepsy community.",
-    ],
-  },
-  missionDetails: {
-    title: "Driving Progress is our Mission",
-    subtitle:
-      "We strive to understand, treat, and ultimately cure epilepsy through open access to multidimensional epilepsy data at scale. The platform provides over 200,000 EEG recordings from diverse contexts including routine outpatient EEGs, critically ill patients, and epilepsy monitoring unit evaluations.",
-    additionalInfo:
-      "As the platform grows, it will also include extensive accompanying clinical data like medications, imaging, genetics, and more from institutions worldwide.",
-    introduction:
-      "Researchers can use Epilepsy.Science to easily build customized cohorts by connecting data points across datasets.The platform enables scientists to publish -- at no cost -- high quality datasets for citation, reuse, and reproducible research.By promoting open science, Epilepsy.Science aims to accelerate discoveries and improve patient outcomes.",
-    highlightsTitle: "This collaboration brings together:",
-    highlights: [
-      "PennSieve's scalable data management and sharing capabilities.",
-      "BDSP's extensive data resources including over 200,000 EEG recordings.",
-      "The AWS Open Data Sponsorship Program covers the cost of storage.",
-      "Epilepsy.Science offers unprecedented opportunities for open, collaborative epilepsy research.",
-    ],
-    imageSrc: "https://images.ctfassets.net/erzgaqq17mnz/25miXcUrrPaHjdX0dJ4ce6/8efe96f5a3723a404f4a6a33dbb70d18/mission-details-image.png",
-    footer: "Epilepsy.Science offers unprecedented opportunities for open, collaborative epilepsy research through its powerful data resources, analytics tools, and cloud-based platform."
-  },
-};
+  ]
+
 </script>
 
 <style scoped lang="scss">
