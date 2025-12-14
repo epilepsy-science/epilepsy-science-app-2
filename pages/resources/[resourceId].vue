8ed63409bfe6008bb679efd8aec15b48add61d3b
vijay kumar, samayamantula <vijay@sparc.science>
2025-12-14 15:02:14 -0500

    remove tools and resources pages and dependencies

--- pages/resources/[resourceId].vue
+++
@@ -1,9 +0,0 @@
-<script>
-export default {
-  async setup() {
-    const route = useRoute()
-    const newPath = route.href.replace('/resources/', '/tools-and-resources/')
-    await navigateTo(newPath, { redirectCode: 301 })
-  },
-}
-</script>
