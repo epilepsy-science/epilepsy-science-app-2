12f1b27e28fca2173dac615e1876b2a809249fbd
vijay kumar, samayamantula <vijay@sparc.science>
2025-12-14 18:14:49 -0500

    remove middleware

--- middleware/authRoute.js
+++
@@ -1,9 +0,0 @@
-import { useMainStore } from '@/store/index'
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  if (process.server) return
-  const user = useMainStore().userProfile
-  // Re-direct the user to the home page if they are not logged in
-  if (user == null) {
-    return navigateTo('/')
-  }
-})
