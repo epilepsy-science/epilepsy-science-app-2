1884abab8cff7a68b05e0afc7535fef890e7ae23
vijay kumar, samayamantula <vijay@sparc.science>
2026-04-03 16:03:28 -0400

    clean up

--- plugins/pinia.js
+++ plugins/pinia.js
@@ -3,7 +3,6 @@ import { useMainStore } from '~/store/index'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const store = useMainStore(nuxtApp.$pinia)
-  await store.init()
   return {
     provide: {
       store: store
